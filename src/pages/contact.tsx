3262bae8c175d79a7bdc4ade6cf0744245a78c0d
Priyanshu <sethipriyanshu@outlook.com>
2024-06-20 04:57:27 -0500

    add content on contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,5 @@
+import contact_img from '../images/contact_img.jpg'; 
+
 export const Contact = () =>{
     return(
         <div>
@@ -19,6 +21,33 @@ export const Contact = () =>{
      
     </article>
   </div>
+      <div className='contact_section'>
+        <img src={contact_img}></img>
+        <div className="text-info">
+                    <h1>Dr. Hee Yun Lee</h1>
+                    <p>Professor & Endowed Academic Chair</p>
+                    <br/>
+                    <b><p>Office:</p></b>
+                    <p>City, State, ZIP</p>
+                    <br/>
+                    <b><p>Lab:</p></b>
+                    <p>City, State, ZIP</p>
+                    <br/>
+                    <p><a href="">email@email.com</a></p>
+                    <p>123456789</p>
+                </div>
+                <div className="map">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.172042453614!2d144.95373631568307!3d-37.81627967975144!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0x5045675218ceed0!2z44CSMzAwMCBWaWN0b3JpYSwg5qCq5byP5Lya56S-55Sw5Yy65LiK55uu77yS5LiB55uu!5e0!3m2!1sja!2sjp!4v1549449757488"
+                        width="100%"
+                        height="300"
+                        frameBorder="0"
+                        style={{ border: 0 }}
+      
+                        aria-hidden="false"
+                    ></iframe>
+                </div>
+      </div>
         </div>
     )
 }
