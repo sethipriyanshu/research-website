d2c5d01f65cab664760ba2b291023100f8d7ec15
Priyanshu <sethipriyanshu@outlook.com>
2024-07-01 07:07:24 -0500

    add 2020-2024 publications

--- src/pages/publications.tsx
+++ src/pages/publications.tsx
@@ -7,18 +7,264 @@ export const Publications = () =>{
       <img
       src="https://picsum.photos/1600/900"
       alt=""
-      style={{ width: '100%', height: '30vh', objectFit: 'cover', objectPosition: 'top' }}
-    />
+      style={{ width: '100%', height: '30vh', objectFit: 'cover', objectPosition: 'top' }}/>
         <div className="text-overlay">
           <div>
               <h1>Publications</h1>
           </div>
       </div>
-
-      </div>
-     
+      </div> 
     </article>
   </div>
+
+<div className="publications_title">
+  <h1>2024</h1>
+</div>
+<div className="publications">
+<p>Love, H.A., Ezemenaka, C., Horton, A. G., Yoon, Y. J., & <b>Lee, H. Y.</b> (In Press). Undergraduate class standing, social support, and depressive symptoms. Journal of American College Health.</p>
+
+<p>Shin, S.H., Walker, S., Ji, H., & <b>Lee, H. Y.</b> (In Press). Performance Under Fire: Older Adult Cognitive Risks and Protections Under Heat Strain, The Gerontologist.</p>
+
+<p>Cosenzo, L. G., Littleton, T., Johnson, K., Albright, D. L., & <b>Lee, H. Y.</b> (In Press). Transportation-related geographic isolation, social connectedness, and depressive symptoms among adults living in the Black Belt region of Alabama: A moderation analysis. Journal of Rural Mental Health.</p>
+
+<p>Han, A., Malone, L., <b>Lee, H. Y.</b>, Gong, J., Henry, R., Zhu, X., & Yuen, H. K. (In Press). The use of ecological momentary assessment for family caregivers of adults with chronic conditions: A systematic review. Health Psychology Research.</p>
+
+<p>Rick, T.J., Sagaram, S., Jewett, P.I., <b>Lee, H. Y.</b>, … Blaes. A. (2024). A pilot randomized controlled trial of an online intervention for Hodgkin lymphoma survivors to increase knowledge about late effects and recommended screening. Journal of Cancer Survivorship. <a href="https://doi.org/10.1007/s11764-024-01587-2">https://doi.org/10.1007/s11764-024-01587-2</a></p>
+
+<p>Choi, Y. J., <b>Lee, H. Y.</b>, Yoon, Y. J., & Blackburn, J. (2024). Health literacy among Korean American immigrant women in the USA: Role of social support. Social Work in Public Health. Published online first. <a href="https://doi.org/10.1080/19371918.2024.2324148">https://doi.org/10.1080/19371918.2024.2324148</a></p>
+
+<p><b>Lee, L. H.</b>, Yoon, Y.J., Kim, D., Noh, H., Jones, S., & <b>Lee, H. Y.</b> (2024). Perceived transportation barriers moderate the association between depressive symptoms and household transportation use: A pilot study. Journal of Transport & Health, 33. <a href="https://doi.org/10.1016/j.jth.2023.101713">https://doi.org/10.1016/j.jth.2023.101713</a></p>
+
+<p><b>Lee, H. Y.</b>, Yoon, Y. J., Choi, Y. J., & Ham, Y-H. (2024). Factors Associated with Korean American Women’s Health-Related Internet Use: Findings from Andersen’s Behavioral Model. Journal of Immigrant and Minority Health, 26, 124–132. <a href="https://doi.org/10.1007/s10903-023-01540-y">https://doi.org/10.1007/s10903-023-01540-y</a></p>
+<br/>
+  </div>
+
+  <div className="publications_title">
+  <h1>2023</h1>
+</div>
+<div className="publications">
+<p>Allen, R. S., McIntyre, A.,D Oliver, J.S., Payne-Foster, P., Cox, B.S. Hay-McCutcheon, M.J., Wilson, L., Spencer, C., & <b>Lee, H. Y.S</b> (2023). Church leaders share and implement solution-focused health strategies during the COVID-19 pandemic in rural Alabama. Journal of Racial and Ethnic Health Disparities.</p>
+
+<p>Eyer, J. C., Won, C. R.,D Sawyer, M., Luo, Y.,D Wang, K.,D Chipalo, E.,D Thomas-LeBlanc, G., & <b>Lee, H. Y.S</b> (2023). A qualitative study of barriers and facilitators to opioid prevention, treatment, and recovery in rural Alabama. Qualitative Health Report, 28(11), 3358-3378.</p>
+
+<p>Lee, J., Kang, D., <b>Lee, H. Y.</b>, & Kim, J. W. (2023). The effects of authoritative source cues and argument strength of correction tweets on MMR vaccine-related misinformation credibility. Health Education Journal.</p>
+
+<p><b>Lee, H. Y.</b>, Chou, C., Wang, K.,D McDaniel, C. C., &, Daniel, C. L. (2023). The roles of HPV awareness and knowledge in HPV vaccine initiation and completion among college students in the Deep South of the United States: Applying the Andersen’s Behavioral Model. International Journal of Health Promotion and Education.</p>
+
+<p><b>Lee, H. Y.</b>, Eyer, J. C., Luo, Y.,D Jeong, H.,D Chapman, S., & Hudnall, M. (2023). Opioid literacy among individuals living in rural Alabama: The role of social determinants of health. Journal of Psychosocial Nursing and Mental Health Services, 1-8.</p>
+
+<p><b>Lee, H. Y.</b>, Song, J., & Choi, E.Y.D (2023). Childhood abuse and opioid prescription use in adulthood: Differences between non-Hispanic Whites and non-Hispanic Blacks in the United States. PLoS ONE, 18(9), e0291752.</p>
+
+<p><b>Lee, H. Y.</b>, Daniel, C.L., Wang, K.,D McLendon, L., & Coyne-Beasley, T. (2023). HPV vaccination among college students in the South: The role of HPV knowledge on vaccine initiation and completion. Asian Pacific Journal of Cancer Prevention, 24(6), 2149–2156.</p>
+
+<p><b>Lee, H. Y.</b>, Hao, Z. & Choi, E.Y.D (2023). Depression among Korean American Immigrants Living in Rural Alabama: Use of Social Determinants of Health Framework. Ethnicity & Health.</p>
+
+<p><b>Lee, H. Y.</b>, Kois, L., Wang, K., Won, C. R.,D Han, A., & Allen, R. S. (2023). Correlations between social determinants of health and technology access among rural African American Alabamians. Journal of Health Care for the Poor and Underserved, 34(1), 21-34.</p>
+
+<p><b>Lee, H. Y.</b>, Luo, Y.,D Chou, C., Lee, M.H.,F & Bennett, M. (2023). Associated factors with colorectal cancer screening awareness in the Black Belt region of Alabama: A comparison among three types of colorectal cancer screening. Journal of Health Disparities Research and Practice, 16(1), Article 4.</p>
+
+<p><b>Lee, H. Y.</b>, Luo, Y.,D Won, C.R.,D Daniel, C., & Coyne-Beasley, T. (2023). HPV and HPV vaccine awareness among African Americans in the Black Belt region of Alabama. Journal of Racial and Ethnic Health Disparities.</p>
+
+<p><b>Lee, H. Y.</b>, Won, C. R.,D Luo, Y.,D & Lee, M. H.F (2023). Human Papillomavirus (HPV) and HPV vaccine awareness among Korean American immigrants in Alabama: Would internet use improve health awareness? Asian Pacific Journal of Cancer Prevention, 24(8), 2575–2581.</p>
+
+<p>Nam, Y., Choi, E.Y.,D Won, C.R.,D & <b>Lee, H.Y.S</b> (2023). The role of racial/ethnic discrimination in financial access and material hardship: Findings from Korean immigrants living in the Deep South. Journal of Consumer Affairs.</p>
+
+<p>Roh, S., <b>Lee, H. Y.</b>, Jun, S. J., Lee, Y-S., & Won, C. R.D (2023). Andersen's Behavioral Model to identify correlates of cervical cancer knowledge among American Indian women. Asian Pacific Journal of Cancer Prevention, 24(4), 1151-1157.</p>
+
+<p>Roh, S., Lee, Y-S., Kenyon, D. B., Elliott, A. J., Petereit, D. G., Gaba, A., & <b>Lee, H. Y.</b> (2023). Mobile web app intervention to promote breast cancer screening among American Indian women in the Northern Plains: Feasibility and efficacy study. JMIR Formative Research, 7, e47851.</p>
+
+<p>Shin, H.R., Um, S.R., Yoon, H.J., Choi, E.Y., Shin, W.C., <b>Lee, H. Y.</b>, & Kim, Y.S. (2023). Comprehensive senior technology acceptance model of daily assistive technology for older adults with frailty: Cross-Sectional study. Journal of Medical Internet Research, 25, e41935.</p>
+
+<p>Suntai, Z., Noh, H., Lee, L., Bell, J. G., Lippe, M. P., & <b>Lee, H. Y.</b> (2023). Quality of Care at the End of Life: Applying the Intersection of Race and Gender. The Gerontologist, gnad012. Advance online publication.</p>
+
+<p>Zhu, X., <b>Lee, H. Y.</b>, & Gong, J. (2023). Examining the construct of HPV vaccine hesitancy and its determinants using Randomized Neural Networks. Smart Health, 28, 100386.</p>
+      <br/>
+
+  </div> 
+
+
+  <div className="publications_title">
+  <h1>2022</h1>
+</div>
+<div className="publications">
+<p>Choi, Y. J., <b>Lee, H. Y.</b>, Yoon, Y. J.,D & Oh, J. (2022). Correlates of mental health literacy among Korean American immigrant women. Social Work in Mental Health, 20(4), 465-482.</p>
+
+<p>Choi, E. Y.,D Kanthawala, S., Kim, Y.S, & <b>Lee, H. Y.,S</b> (2022). Urban/Rural digital divide persists in older adults: Does it vary racial/ethnic groups? Journal of Applied Gerontology, 7334648211073605. Advance online publication.</p>
+
+<p>Choi, E. Y.,D Shin, H., Kim, S., <b>Lee, H. Y.</b>, & Kim, Y. S. (2022). Limited health literacy increases risk of frailty among community‐dwelling older adults: Longitudinal findings from the Korean Frailty and Aging Cohort Study. Geriatrics & Gerontology International, 22, 325-331.</p>
+
+<p>Gajos, J., Luo, Y.,D Wang, K.,D & <b>Lee, H. Y.S</b> (2022). Self-Rated health and mental health among African Americans in the rural south: The influence of social determinants of health and race-related stressors. Journal of Healthcare for the Poor and Underserved, 33(3), 1305–1321.</p>
+
+<p>Hopson, L. M., Lidbe, A., Jackson, M. S., Adanu, E., Li, X., Penmetsa, P., <b>Lee, H. Y.</b>, Anderson, A., Obuya, C., & Meerdink, G. (2022). Transportation to school and academic outcomes: A systematic review. Educational Review.</p>
+
+<p>Ji, H., Shin, S. H., Rogers, A., Neese, J.,M & <b>Lee, H. Y.S</b> (2022) Racial/Ethnic disparities in drug use during the COVID 19 pandemic: Moderating effects of the access to non-profit substance use disorder treatment services. PLoS ONE, 17(6): e0270238.</p>
+
+<p>Kayser, K., Washington, A., Anderson, G., Harris, L. M., <b>Lee, H. Y.</b>, & LaJoie, A. S. (2022). Perceived barriers and facilitators in accessing cervical cancer screening: The voices of women in a low-income urban community. Family & Community Health, 45(1), 46-57.</p>
+
+<p><b>Lee, H. Y.</b>, Luo, Y.,D Daniel, C., Wang, K.,D & Ikenberg, C.D.D (2022). Is HPV vaccine awareness associated with HPV knowledge level?: Findings from HINTs data. Ethnicity & Health, 27(5), 1166-1177. Advance online publication.</p>
+
+<p><b>Lee, H. Y.</b>, Noh, H., Choi, E., & Lee, L. H. (2022). Social determinants of willingness to discuss end-of-life care with family and doctors among Korean American immigrants: Findings from a cross-sectional survey in Alabama. Health & Social Care in the Community.</p>
+
+<p><b>Lee, H. Y.</b>, Wang, K.,D Choi, E. Y.,D Gajos, J. M., & Won, C. R.D (2022). Opioid knowledge among African Americans living in rural Alabama: Findings from a social determinants of health framework. Journal of Drug Issues.</p>
+
+<p><b>Lee, H. Y.</b>, Wang, K.,D Johnson, K., Kubanga, K.,D Han, A., & Choi, E.D (2022). Does the use of social media affect the online health information-seeking behaviors among underserved African Americans in rural Alabama? Journal of Health Care for the Poor and Underserved, 33(4), 1949–1964.</p>
+
+<p><b>Lee, H. Y.</b>, Xiong, S., Khang, T., Vue, B., Culhane-Pera, K-A., Pergament, S., Torres, M. B., Sur, A., Koopmeiners, J.S., & Desai, J. (2022). Evaluating HmongHPV eHealth to Promote Hmong Adolescent HPV Vaccinations: A Pilot Feasibility Study. JMIR Formative Research.</p>
+
+<p>Lee, M.H.,F Schwartz, A., Luo, Y.D & <b>Lee, H.Y.</b> (2022). Mammogram uptake among Korean American women in the South: Do health beliefs matter? Asian Pacific Journal of Cancer Prevention, 23(11).</p>
+
+<p>Noh, H., <b>Lee. H. Y.</b>, Lou, Y.,D & Lee, L. H. (2022). Awareness of hospice care among rural African Americans: Findings from social determinants of health framework. American Journal of Hospice and Palliative Medicine, 39 (7), 822-830.</p>
+
+<p>Noh, H., <b>Lee. H. Y.</b>, Lou, Y.,D & Lee, L. H. (2022). Willingness to Discuss End-of-Life Care Wishes among Rural African Americans. The Journal of Applied Gerontology, 41 (7), 1763-1772.</p>
+
+<p>Oh, A., Gaysynsky, A., Winer, R. L., <b>Lee, H. Y.</b>, Brewer, N. T., & White, A. (2022). Considerations and opportunities for multilevel HPV vaccine communication interventions. Translational Behavioral Medicine, 12(2), 343-349.</p>
+
+
+  </div> 
+
+
+  <div className="publications_title">
+  <h1>2021</h1>
+</div>
+<div className="publications">
+<p>Baik, J., Yoon, Y. J.,D Gibson, P., Lo, N., Nam, H. E., Im, Y. J., & <b>Lee, H. Y.</b> (2021). Mothering and mothered during defection and resettlement: Experiences of North Korean refugee women and their children. Children and Youth Services Review, 130, 106242.</p>
+
+<p>Chung, S.D., <b>Lee, H. Y.</b>, Lee, M.,F & Chung, S. (2021). Health literacy in Korean adults and Korean American immigrants: Implications for achieving health equity. International Quarterly of Community Health Education, 42(1), 29-36.</p>
+
+<p>Daniel, C., Lawson, F., Vickers, M., Green, C., Wright, W., Coyne-Beasley, T., <b>Lee, H. Y.</b>, & Turberville, S. (2021). Enrolling a rural, community pharmacy as a vaccines for children provider to increase HPV vaccination: A feasibility study. BMC Public Health, 21, 1266.</p>
+
+<p>Guo, Y.,F Qin, W., & <b>Lee H.Y.</b> (2021). Social participation and depressive symptoms in older African American cancer survivors: Comparison with the non-cancer subjects. Aging & Mental Health.</p>
+
+<p>Han, A., Yuen, H. K., & Jenkins, J., & <b>Lee, H. Y.</b> (2021). Acceptance and commitment therapy (ACT) guided online for distressed caregivers of persons living with dementia. Clinical Gerontologist.</p>
+
+<p>Jin, S.W.,F Lee, J., & <b>Lee, H. Y.</b> (2021). Analyzing factors associated with decisional stage of adopting breast cancer screening among Korean American women using precaution adoption process model, Ethnicity & Health, 1-17. DOI: 10.1080/13557858.2018.1520813.</p>
+
+<p>Li, Q.,D <b>Lee, H. Y.</b>, Jin, S. W.,F & Lee, D.U (2021). Advancing health literacy: Gender- and age-specific approaches to reducing health disparity. Best Practice in Mental Health, 17(1), 56-70.</p>
+
+<p><b>Lee, H.Y.</b>, Lee, L., Luo, Y.,D Penmetsa, P., Adanu, E.K., Li, X., & Jones, S. (2021). Anxiety and depression among African-Americans living in rural Black Belt areas of Alabama: Use of social determinants of health framework. British Journal of Social Work, bcab199.</p>
+
+<p><b>Lee, H.Y.</b>, Choi, E.Y.,D Won, C.R.,D Song, J., & Gajos, J.M. (2021). Opioid literacy of Korean Americans in Rural Alabama: Addressing the role of social determinants of health. Journal of Transcultural Nursing, 32(6), 727-739.</p>
+
+<p><b>Lee, H.Y.</b>, Li, Q.,D Luo, Y.,D Wang, K.,D Hendrix, S.,D Lee, J., Yoon, S., & Huy. N. (2021). Is Pap test awareness critical to Pap test uptake in women living in rural Vietnam? Asian Pacific Journal of Cancer Prevention, 22(3), 903-908.</p>
+
+<p><b>Lee, H.Y.</b>, Hao, Z., & Choi E. Y.D (2021). Online health information-seeking behavior among Korean American immigrants in rural Alabama: Dose discrimination matter? Journal of Immigrant and Minority Health, 24(4), 996-1004.</p>
+
+<p><b>Lee, H.Y.</b>, Kim, S. Y., Neese, J.,M & Lee, M. H.F (2021). Does health literacy affect the uptake of annual physical check-ups?: Results from the 2017 US health information national trends survey. Archives of Public Health, 79(1), 38.</p>
+
+<p><b>Lee, H.Y.</b>, Jin, S. W.,F Henning-Smith, C., & Lee, J. (2021). Role of health literacy in health-related information-seeking behavior online: Cross-sectional study. Journal of Medical Internet Research, 23(1), e14088.</p>
+
+<p><b>Lee, H.Y.</b>, Kanthawala, S., Choi, E. Y.,D & Kim, Y.S. (2021). Rural and non-rural digital divide persists in older adults: Internet access, usage, and attitudes toward technology. Gerontechnology 20(2), 1-9.</p>
+
+<p><b>Lee, H.Y.</b>, Luo, Y.,D Neese, J.,M Daniel, C., & Hahm, H. (2021). The role of English proficiency in HPV and HPV vaccine awareness: A cross-sectional study across race/ethnicity. Asian Pacific Journal of Cancer Prevention, 22(2), 349–357.</p>
+
+<p><b>Lee, H.Y.</b>, Luo, Y.,D Neese, J.,M Lee, D., & Daniel, C. (2021). Targeted intervention strategies to promote HPV-associated cancer equity. Health Behavior and Policy Review, 8(2), 100-109.</p>
+
+<p><b>Lee, H.Y.</b>, Won, C. R.,D Hao, Z.,D Luo, Y.,D Allen, R., & Jones, S. (2021). Attitude and responses to the pandemic and COVID – 19 vaccination in rural Alabama: The importance of transportation. Best Practice in Mental Health, 17(1), 1-17.</p>
+
+<p>Li, X., Hu, Q., Liu, J., Nambisan, S., Khattak, A., Lidbe, A., & <b>Lee, H. Y.</b> (2021). Pathway analysis of relationships among community development, active travel behavior, body mass index, and self-rated health. International Journal of Sustainable Transportation.</p>
+
+<p>Kimball, B.C., Kudek, M.R., Ly, D.V., Eagle, S., Hausman, V., <b>Lee, H. Y.</b>, & Sadak, K.T. (2021). Empowering childhood cancer survivors through integrated, individualized health insurance education. Precision Cancer Medicine, 4.</p>
+
+<p>McDaniel, C. C., Hallam, H. H., Cadwallader, T., <b>Lee, H. Y.</b>, & Chou, C. (2021). Persistent racial disparities in cervical cancer screening with Pap test. Preventive Medicine Reports, 24 (101652).</p>
+
+<p>McDaniel, C. C., Hallam, H. H., Cadwallader, T., <b>Lee, H. Y.</b>, & Chou, C. (2021). Disparities in cervical cancer screening with HPV test among females with diabetes in the Deep South. Cancers, 13(24), 6319.</p>
+
+<p>Shin, H-R., Choi, E. Y., Kim, S-K., <b>Lee, H. Y.</b>, & Kim, Y. S. (2021). Health literacy and frailty in community-dwelling older adults: Evidence from a nationwide cohort study in South Korea. International Journal of Environmental Research and Public Health, 18(15), 7918.</p>
+
+<p>Xiong, S.,D Kasouaher, M. Y., Vue, B., Culhane-Pera, K. A., Pergament, S. L., Desai, J., Torrez, M. B., & <b>Lee, H. Y.S</b> (2021). “We will do whatever it takes”: Perceptions of Human Papillomavirus Vaccinations in Hmong-American adolescents and parents. Journal of Cancer Education, 23, 1-9.</p>
+
+<p>Yoon, S., Wang, K.,D Luo, Y.,D Lee, J., Neese, J.,M & <b>Lee, H.Y.S</b> (2021). Cancer screening literacy among Vietnamese population: Does annual checkup improve cancer screening literacy? Asian Pacific Journal of Cancer Prevention, 22(3), 927-933.</p>
+
+<p><b>Lee, H.Y.</b>, Choi, Y. J., Shin, J.H.,M Yoon, Y. J.,D & An, S.O.D (2021). Adherence to Cervical Cancer Screening in Korean American Immigrant Women: Identifying Malleable Variables for Intervention Development. Journal of Transcultural Nursing, 32(3), 230-238. DOI: 10.1177/1043659620914693. PMID: 32336226.</p>
+ <br/>
+  </div> 
+
+
+  <div className="publications_title">
+  <h1>2020</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+  <div className="publications_title">
+  <h1>2019</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+
+  <div className="publications_title">
+  <h1>2018</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+  <div className="publications_title">
+  <h1>2017</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+
+  <div className="publications_title">
+  <h1>2016</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+  <div className="publications_title">
+  <h1>2015</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+  <div className="publications_title">
+  <h1>2014</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+  <div className="publications_title">
+  <h1>2013</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+  <div className="publications_title">
+  <h1>2012</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+  <div className="publications_title">
+  <h1>2011</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+  <div className="publications_title">
+  <h1>2010</h1>
+</div>
+<div className="publications">
+
+  </div> 
+
+  <div className="publications_title">
+  <h1>1994 - 2009</h1>
+</div>
+<div className="publications">
+
+  </div> 
         </div>
     )
 }
