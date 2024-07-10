bbbaa7f3a450aae43add2726ca9e29c09ceb12bc
Priyanshu <sethipriyanshu@outlook.com>
2024-07-10 10:19:19 -0500

    add global engagement content

--- src/pages/global-engagement.tsx
+++ src/pages/global-engagement.tsx
@@ -22,6 +22,69 @@ export const GlobalEngagement = () =>{
      
     </article>
   </div>
+
+  <div className="publications_title">
+  <h1>International Projects</h1>
+</div>
+<div className="publications">
+<p><b>Lee, H.Y.</b> (2019-2027). Co-Investigator. 300,000,000 Won (equivalent to $300,000). 
+Technology Adoption and Acceptance in Older Adults and Its Use for Chronic Diseases 
+Intervention (PI: Kim, Young Sun, Ph.D.). Funded by the National Research Foundation of 
+Korea, Seoul, Korea. (Written in Korean)</p>
+
+<p><b>Lee, H.Y.</b> (2015-2019). Co-Investigator. Frailty cohort construction and development of frailty 
+    prevention intervention in Korea (PI: Won, Jang Won, M.D., Kyunghee University 
+    School of Medicine). 1,200,000,000 Won (equivalent to $12.0M). Funded by the 
+    National Research Foundation of Korea, Seoul, Korea. (Written in Korean)</p>
+
+<p><b>Lee, H.Y.</b> (2017-2018). Co-Investigator. Developing the Technology of Personalized Mobility 
+    Chain for Elderly. 50,000,000 won (equivalent to $50,000). Funded by the Ministry of 
+    Health and Welfare. (Written in Korean)</p>
+
+<p><b>Lee, H.Y.</b> (2015-2018). Co-Investigator. 300,000,000 Won (equivalent to $300,000).
+    Development of Dementia Health Literacy Intervention Programs in Korea (PI: Kim, 
+    Young Sun, Ph.D., Kyunghee University Graduate School of Gerontology). Funded by the Korea Health Industry Development Institute, Seoul, Korea. (Written in Korean)</p>
+
+<p><b>Lee, H.Y.</b>(2016-2017). Co-Investigator. 90,000,000 Won (equivalent to $90,000). Developing 
+    the Technology of Personalized Mobility-Assistance-Service Chain for the Elderly (PI: 
+    Kim, Young Sun, Ph.D., Kyunghee University Graduate School of Gerontology). Funded 
+    by the National Research Foundation of Korea, Seoul, Korea. (Written in Korean)</p>
+
+<p><b>Lee, H.Y.</b>(2013-2016). Co-Investigator. 300,000,000 Won (equivalent to $300,000).
+    Health literacy in Korea: Differences across age and gender groups (PI: Soondul 
+    Chung, Ph.D., Ehwa Women’s University School of Social Work). Funded by the 
+    National Research Foundation of Korea (NFR-2013-S1A3A2055071). (Written in 
+    Korean)</p>
+
+<p><b>Lee, H.Y.</b>(2013-2014). Co-Investigator. 20,000,000 Won (equivalent to $20,000). Health 
+    literacy and its impact on health disparity among people with disability in Korea: New 
+    directions toward health equity (PI: Byunghyun Park, Ph.D., Pusan National University School of Social Work). Funded by the Rehabilitation Center of Busan Metropolitan City. Busan, Korea. (Written in Korean)</p>
+
+<p><b>Lee, H.Y.</b> (2013-2014). Co-Investigator. 50,000,000 Won (equivalent to $50,000). Exploratory 
+    study on health literacy promotion for the multi-cultural family (PI: Byunghyun Park, Ph.D., Pusan National University School of Social Work). Funded by the Korea Medical Institute, Seoul, Korea. (Written in Korean)</p>
+
+<p><b>Lee, H.Y.</b> (2012-2013). Co-Investigator. 20,000,000 Won (equivalent to $20,000). Health 
+    literacy of older adults in Korea (PI: Byunghyun Park, Ph.D., Pusan National 
+    University School of Social Work). Funded by the Korean Ministry of Health and 
+    Welfare, Seoul, Korea. (Written in Korean)</p>
+
+<p><b>Lee, H.Y.</b> (2011-2012). $1500. 2011 Trek/Transform one year long technology training for 
+    teaching, College of Education and Human Development, University of Minnesota.</p>
+
+<p><b>Lee, H.Y.</b>(Summer, 2010). $3,000. Travel grant to Hong Kong, China, to attend the 2010 Joint 
+    World Conference on Social Work and Social Development: The Agenda. Funded by the Office of International Programs, University of Minnesota and matching travel fund by the School of Social Work, University of Minnesota.</p>
+
+<p><b>Lee, H.Y.</b> (Summer, 2009). $4,000. Annual conference support. A partnership approach to
+    promote health and mental health literacy and reduce disparities among immigrant and 
+    refugee communities. Funded by Blue Cross/Blue Shield.</p>
+
+<p><b>Lee, H.Y.</b>(Summer, 2007). $3,000. Travel grant to Seoul, South Korea, for the purpose of 
+    building a network for international research collaboration with faculty at the Seoul  
+    National University. Funded by the Office of International Programs, University of 
+    Minnesota and matching travel grant by the School of Social Work, University of 
+    Minnesota.</p>
+    <br/>
+</div>
         </div>
     )
 }
