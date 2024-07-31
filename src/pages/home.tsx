6aadd73c4b6b7c0d4a46251bb1e7a8e910fafd20
Priyanshu <sethipriyanshu@outlook.com>
2024-07-31 14:24:51 -0500

    add about section on home

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,6 +2,7 @@ import { SecondaryNavbar } from "../components/secondary-navbar";
 import homeImage1 from "../images/home01.gif";
 import homeImage2 from "../images/home02.png";
 import back from '../images/back.jpg'; 
+import contact_img from '../images/contact.jpg';
 
 export const Home = () =>{
   const downloadPdf = () => {
@@ -28,7 +29,6 @@ export const Home = () =>{
               <h1 style={{fontSize:'40px'}}>Hee Lab</h1>
               <p style={{fontSize:'20px'}}>The School of Social Work</p>
               <div className="ua_component_wrapper ua_custom-class">
-    <button className="ua_cta" onClick={downloadPdf}>Download my CV</button>
   </div>
             <SecondaryNavbar/>
           </div>
@@ -49,9 +49,38 @@ export const Home = () =>{
         stress-induced transcriptome changes, and (6) Technology development for single-cell and subcellular
         studies of spatial transcriptome and proteome.
       </p>
-      <div className="home-images">
-        <img src={homeImage1} alt="Description of image 1" />
-        <img src={homeImage2} alt="Description of image 2" />
+
+
+    <div className='contact_section'>
+        <img src={contact_img}></img>
+        <div className="text-info">
+                    <h1>Dr. Hee Yun Lee</h1>
+                    <p>Professor & Endowed Academic Chair</p>
+                    <br/>
+                    <b><p>Office:</p></b>
+                    <p>1022 Little Hall, Box 870314, Tuscaloosa, Alabama</p>
+                    <br/>
+                    <p><a href="" style={{color:"blue"}}>hlee94@ua.edu</a></p>
+                    <p>205-348-6553</p>
+                    <br />
+                    <div className="ua_component_wrapper ua_custom-class">
+    <button className="ua_cta" onClick={downloadPdf}>Download my CV</button>
+  </div>
+                </div>
+
+                <div className="map">
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3338.2110499828627!2d-87.54804012485982!3d33.20856286180825!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x888602993b0ccf9b%3A0xbb276ece861283db!2sLittle%20Hall%2C%20Tuscaloosa%2C%20AL%2035401!5e0!3m2!1sen!2sus!4v1721763943549!5m2!1sen!2sus"
+                        width="100%"
+                        height="300"
+                        frameBorder="0"
+                        style={{ border: 0 }}
+      
+                        aria-hidden="false"
+                    ></iframe>
+                </div>
+                
+              
       </div>
     </div>
 
